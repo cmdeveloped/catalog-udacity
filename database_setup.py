86e0fd82f399c34c737c0048c6f29f307f2cd2a4
cmdeveloped <cmdeveloped@gmail.com>
2017-06-16 01:32:48 +0000

    Add database setup file to project folder and setup configuration, classes, and class mappers for our two tables needed.

---
+++ database_setup.py
@@ -0,0 +1,36 @@
+# Configuration for DB
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+# Creating Class definitions for DB
+
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    # Mappers for our class category
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+
+class CategoryItem(Base):
+
+    __tablename__ = 'category_item'
+
+    # Mappers for our class category 
+    name = Column(String(80), nullable = False)
+    id = Column(Integer, primary_key = True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
