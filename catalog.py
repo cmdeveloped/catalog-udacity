0542a7962a100f011b768d31183bee1affea2db7
cmdeveloped <cmdeveloped@gmail.com>
2017-06-18 21:21:09 +0000

    Fix routing to edit and delete pages using flask.

--- catalog.py
+++ catalog.py
@@ -12,11 +12,24 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/catalog')
-def mainCatalog():
-    category = session.query(Category).first()
+@app.route('/catalog/<int:category_id>/')
+
+def mainCatalog(category_id):
+    category = session.query(Category).filter_by(id = category_id).one()
     items = session.query(CategoryItem).filter_by(category_id = category.id)
     return render_template('catalog.html', category = category, items = items)
+
+@app.route('/catalog/<int:category_id>/new/')
+def newCategoryItem(category_id):
+    return "Page to create a new Category Item."
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/')
+def editCategoryItem(category_id, item_id):
+    return "Page to edit a menu item."
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/')
+def deleteCategoryItem(category_id, item_id):
+    return "Page to delete a menu item."
 
 
 if __name__ == '__main__':
