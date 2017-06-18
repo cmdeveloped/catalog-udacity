e3687b0b83dd35f58cf677ff6275d1e23a2de4ca
cmdeveloped <cmdeveloped@gmail.com>
2017-06-18 22:41:06 +0000

    Create templates for new, edit, and delete catalog items. Also updated catalog.py file.

--- catalog.py
+++ catalog.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem
+
+app = Flask(__name__)
 
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.bind = engine
@@ -13,23 +13,43 @@ session = DBSession()
 
 @app.route('/')
 @app.route('/catalog/<int:category_id>/')
-
 def mainCatalog(category_id):
     category = session.query(Category).filter_by(id = category_id).one()
     items = session.query(CategoryItem).filter_by(category_id = category.id)
     return render_template('catalog.html', category = category, items = items)
 
-@app.route('/catalog/<int:category_id>/new/')
+@app.route('/catalog/<int:category_id>/new/', methods=['GET','POST'])
 def newCategoryItem(category_id):
-    return "Page to create a new Category Item."
+    if request.method == 'POST':
+        newGame = CategoryItem(name = request.form['name'], category_id = category_id)
+        session.add(newGame)
+        session.commit()
+        return redirect(url_for('mainCatalog', category_id=category_id))
+    else:
+        return render_template('newgame.html', category_id=category_id)
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/edit/')
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/', methods=['GET','POST'])
 def editCategoryItem(category_id, item_id):
-    return "Page to edit a menu item."
+    editedItem = session.query(CategoryItem).filter_by(id = category_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('mainCatalog', category_id = category_id))
+    else:
+        return render_template('editgame.html', category_id = category_id, item_id = item_id, i = editedItem)
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/delete/')
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/', methods=['GET','POST'])
 def deleteCategoryItem(category_id, item_id):
-    return "Page to delete a menu item."
+    deletedItem = session.query(CategoryItem).filter_by(id = category_id).one()
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('mainCatalog', category_id = category_id))
+    else:
+        return render_template('deletegame.html', i = deletedItem)
+
 
 
 if __name__ == '__main__':
