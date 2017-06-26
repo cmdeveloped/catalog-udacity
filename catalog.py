413a0b74cabe81c97e1c731f24834cef367f5ea3
cmdeveloped <cmdeveloped@gmail.com>
2017-06-26 09:55:13 +0000

    Display logout button and changed link width. Fixed code to be pep8 worthy.

--- catalog.py
+++ catalog.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for
+from flask import flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CategoryItem
 
 # Import for Secure Access
 from flask import session as login_session
-import random, string
+import random
+import string
 
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
@@ -16,7 +18,8 @@ import json
 from flask import make_response
 import requests
 
-CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
 
 app = Flask(__name__)
 
@@ -26,13 +29,16 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
 # Creating a state token to prevent request forgery
 # Storing said token in the flask session for later validation from user
 @app.route('/login/')
 def showLogin():
-    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    state = ''.join(random.choice(
+        string.ascii_uppercase + string.digits) for x in range(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
+
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
@@ -87,14 +93,15 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(
+            json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
     # Store the access token in the session for later use.
     login_session['credentials'] = credentials.access_token
-    credentials = AccessTokenCredentials(login_session['credentials'], 'user-agent-value')
+    credentials = AccessTokenCredentials(
+        login_session['credentials'], 'user-agent-value')
     login_session['gplus_id'] = gplus_id
     response = make_response(json.dumps('Successfully connected user.', 200))
 
@@ -115,16 +122,19 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 200px; height: 200px; border-radius: 100%;-webkit-border-radius: 100%;-moz-border-radius: 100%;"> '
+    output += ' " style = "width: 200px; height: 200px; border-radius: 100%;'
+    output += '-webkit-border-radius: 100%;-moz-border-radius: 100%;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print ("done!")
     return output
+
 
 @app.route('/gdisconnect/')
 def gdisconnect():
     credentials = login_session.get('credentials')
     if credentials is None:
-        response = make_response(json.dumps('Current user not connected.'), 401)
+        response = make_response(
+            json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -144,7 +154,8 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+        response = make_response(
+            json.dumps('Failed to revoke token for given user.'), 400)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -152,33 +163,39 @@ def gdisconnect():
 # JSON routes / API Endpoint using a GET request
 @app.route('/catalog/<int:category_id>/games/JSON')
 def catalogGamesJSON(category_id):
-    category = session.query(Category).filter_by(id = category_id).one()
-    items = session.query(CategoryItem).filter_by(category_id = category_id).all()
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(
+        CategoryItem).filter_by(category_id=category_id).all()
     return jsonify(CategoryItems=[i.serialize for i in items])
+
 
 @app.route('/catalog/<int:category_id>/game/<int:game_id>/JSON')
 def catalogGameJSON(category_id, game_id):
-    catalogGame = session.query(CategoryItem).filter_by(id = game_id).one()
-    return jsonify(CatalogGame = catalogGame.serialize)
+    catalogGame = session.query(CategoryItem).filter_by(id=game_id).one()
+    return jsonify(CatalogGame=catalogGame.serialize)
+
 
 @app.route('/')
 @app.route('/catalog')
 def mainView():
     catalog = session.query(Category).all()
-    return render_template('catalog.html', catalog = catalog)
+    return render_template('catalog.html', catalog=catalog)
+
 
 @app.route('/catalog/<int:category_id>/')
 def eachCatalog(category_id):
-    category = session.query(Category).filter_by(id = category_id).one()
-    items = session.query(CategoryItem).filter_by(category_id = category.id)
-    return render_template('categories.html', category = category, items = items)
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(CategoryItem).filter_by(category_id=category.id)
+    return render_template('categories.html', category=category, items=items)
 
-@app.route('/catalog/<int:category_id>/new/', methods=['GET','POST'])
+
+@app.route('/catalog/<int:category_id>/new/', methods=['GET', 'POST'])
 def newCategoryItem(category_id):
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
-        newGame = CategoryItem(name = request.form['name'], category_id = category_id)
+        newGame = CategoryItem(
+            name=request.form['name'], category_id=category_id)
         session.add(newGame)
         session.commit()
         flash('New Game Created!')
@@ -186,11 +203,13 @@ def newCategoryItem(category_id):
     else:
         return render_template('newgame.html', category_id=category_id)
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/edit/', methods=['GET','POST'])
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/edit/',
+           methods=['GET', 'POST'])
 def editCategoryItem(category_id, item_id):
     if 'username' not in login_session:
         return redirect('/login')
-    editedItem = session.query(CategoryItem).filter_by(id = item_id).one()
+    editedItem = session.query(CategoryItem).filter_by(id=item_id).one()
     if request.method == 'POST':
         if request.form['name']:
             editedItem.name = request.form['name']
@@ -203,26 +222,28 @@ def editCategoryItem(category_id, item_id):
         session.add(editedItem)
         session.commit()
         flash('Game description has been edited!')
-        return redirect(url_for('eachCatalog', category_id = category_id))
+        return redirect(url_for('eachCatalog', category_id=category_id))
     else:
-        return render_template('editgame.html', category_id = category_id, item_id = item_id, i = editedItem)
+        return render_template('editgame.html', category_id=category_id,
+                               item_id=item_id, i=editedItem)
 
-@app.route('/catalog/<int:category_id>/<int:item_id>/delete/', methods=['GET','POST'])
+
+@app.route('/catalog/<int:category_id>/<int:item_id>/delete/',
+           methods=['GET', 'POST'])
 def deleteCategoryItem(category_id, item_id):
     if 'username' not in login_session:
         return redirect('/login')
-    deletedItem = session.query(CategoryItem).filter_by(id = item_id).one()
+    deletedItem = session.query(CategoryItem).filter_by(id=item_id).one()
     if request.method == 'POST':
         session.delete(deletedItem)
         session.commit()
         flash('Game has been deleted!')
-        return redirect(url_for('eachCatalog', category_id = category_id))
+        return redirect(url_for('eachCatalog', category_id=category_id))
     else:
-        return render_template('deletegame.html', i = deletedItem)
-
+        return render_template('deletegame.html', i=deletedItem)
 
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host='0.0.0.0', port=5000)
